7a00b47e9a88aac3eba3ed0b5740c11215ea3d6e
ziyom <48964740+ziyom-dev@users.noreply.github.com>
2021-06-06 18:18:45 +0500

    test

---
+++ app.js
@@ -0,0 +1,31 @@
+const sr = ScrollReveal();
+
+sr.reveal(".from-left", {
+    origin: "left",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-right", {
+    origin: "right",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-bottom", {
+    origin: "bottom",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-top", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-center", {
+    scale: 0,
+    duration: 2000,
+    reset: true
+});
