7d7720d0521ac5b35c502189fda135b525baaa23
ziyom <48964740+ziyom-dev@users.noreply.github.com>
2021-06-07 19:08:09 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-const sr = ScrollReveal();
+$.fn.toggleAttr = function(attr, attr1, attr2) {
+    return this.each(function() {
+        var self = $(this);
+        if (self.attr(attr) == attr1)
+            self.attr(attr, attr2);
+        else
+            self.attr(attr, attr1);
+    });
+};
 
-sr.reveal(".from-left", {
-    origin: "left",
-    distance: "50px",
-    duration: 2000,
-    reset: true
-});
-sr.reveal(".from-right", {
-    origin: "right",
-    distance: "50px",
-    duration: 2000,
-    reset: true
-});
-sr.reveal(".from-bottom", {
-    origin: "bottom",
-    distance: "50px",
-    duration: 2000,
-    reset: true
-});
-sr.reveal(".from-top", {
-    origin: "top",
-    distance: "50px",
-    duration: 2000,
-    reset: true
-});
-sr.reveal(".from-center", {
-    scale: 0,
-    duration: 2000,
-    reset: true
+var arrLang = {
+    'uz': {
+        'thisLang': 'Uz',
+        'leftAbout': 'Endi buning uchun biz bor! Biz mutlaqo bepul xizmat va biz reytinglarni sotmaymiz!',
+        'rightAbout': 'Shifokor yoki amaliyotni qidiryapsizmi? Sinovlarni qaerdan olish kerakligini bilmaymanmi? Uy yoki ish yonida qidiryapsizmi? Narxlarni taqqoslashdan charchadingizmi?',
+        'tryBtn': 'Sinab korish',
+        'socialTitle': 'Sog\'liqni saqlash masalalari bilan qiziqasizmi?<br> bizning blog o\'qish! Biz bilan oson, qiziqarli va qiziqarli!',
+        'footerTitle': 'Sizning sog\'lig\'ingizga g\'amxo\'rlik qilamiz!',
+    },
+    'ru': {
+        'thisLang': 'Ру',
+        'leftAbout': 'Теперь для этого есть мы!<br> Мы-абсолютно бесплатный сервис и мы не продаем рейтинги!',
+        'rightAbout': 'Ищете врача или процедуру?<br> Не знаете где сдать анализы? Ищете около дома или работы? Устали сравнивать цены?',
+        'tryBtn': 'Попробовать',
+        'socialTitle': 'Интересуетесь темами про здоровье?<br> Читайте наш блог! С нами легко,весело и интересно!',
+        'footerTitle': 'Мы заботимся о вашем здоровье!',
+    }
+}
+
+$(function() {
+    $('.translate').click(function() {
+        var lang = $("html").attr('lang');
+        $('html').toggleAttr('lang', 'uz', 'ru');
+
+        $('.lang').each(function(index, item) {
+            $(this).html(arrLang[lang][$(this).attr('key')]);
+        });
+    });
 });
