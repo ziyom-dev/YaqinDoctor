592fbdf1bec7096ae6f9eb65413684acf9f61f41
ziyom <48964740+ziyom-dev@users.noreply.github.com>
2021-06-08 11:34:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+const sr = ScrollReveal();
+
+sr.reveal(".from-left", {
+    origin: "left",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-right", {
+    origin: "right",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-bottom", {
+    origin: "bottom",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-top", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    reset: true
+});
+sr.reveal(".from-center", {
+    scale: 0,
+    duration: 2000,
+    reset: true
+});
+
 $.fn.toggleAttr = function(attr, attr1, attr2) {
     return this.each(function() {
         var self = $(this);
